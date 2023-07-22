13d82d7d7f52306fb0867bb3a6a166fe9fc27eda
Saqib Kamal <saqibkamal1994@gmail.com>
2023-07-22 11:52:36 +0000

    copilot third commmit

--- comments.js
+++ comments.js
@@ -1,44 +1,74 @@
-// create a web server with node.js
-// 1. load http module
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var qs = require('querystring');
+// Create web server
+var express = require('express');
+var app = express();
+// Create a server
+var server = require('http').createServer(app);
+// Create a socket.io instance
+var io = require('socket.io').listen(server);
+// Store people online
+var people = {};
+// Store comments
+var comments = [];
+// Store users
+var users = [];
 
-// 2. create http server
-var server = http.createServer(function(request, response) {
-    var parsedUrl = url.parse(request.url);
-    var resource = parsedUrl.pathname;
-    console.log('resource = ' + resource);
+// Listen on port 3000
+server.listen(3000);
 
-    // if client request '/', response index.html
-    if(resource == '/') {
-        var html = fs.readFileSync('index.html', 'utf-8');
-        response.writeHead(200, {'Content-Type':'text/html'});
-        response.end(html);
-    } else if(resource == '/comments') {
-        if(request.method == 'POST') {
-            console.log('POST request');
-            request.on('data', function(data) {
-                var parsedQuery = qs.parse(data.toString());
-                var comment = parsedQuery['comment'];
-                console.log('comment = ' + comment);
-                response.writeHead(200, {'Content-Type':'text/html'});
-                response.end('var1 = ' + comment);
-            });
-        } else {
-            console.log('GET request');
-            var html = fs.readFileSync('comment.html', 'utf-8');
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.end(html);
+// If user connect
+io.sockets.on('connection', function(socket) {
+    // Listen to event 'join' when user join the chat
+    socket.on('join', function(name) {
+        // Store the name in the socket session for this client
+        socket.username = name;
+        // Add the client's username to the global list
+        people[socket.username] = socket;
+        // Push to users
+        users.push(name);
+        // Send to client
+        socket.emit('update', 'You have connected to the server.');
+        // Send to all clients except sender
+        socket.broadcast.emit('update', name + ' has joined the server.');
+        // Send to all clients
+        io.sockets.emit('update-people', users);
+    });
+
+    // Listen to event 'send' when user send message
+    socket.on('send', function(msg) {
+        // Send to all clients except sender
+        socket.broadcast.emit('chat', socket.username, msg);
+    });
+
+    // Listen to event 'disconnect' when user disconnect
+    socket.on('disconnect', function() {
+        // Remove the username from global usernames list
+        delete people[socket.username];
+        // Remove the user from users
+        for (var i = 0; i < users.length; i++) {
+            if (users[i] === socket.username) {
+                users.splice(i, 1);
+            }
         }
-    } else {
-        response.writeHead(404, {'Content-Type':'text/html'});
-        response.end('404 Not Found');
-    }
+        // Update the list of users in chat, client-side
+        io.sockets.emit('update-people', users);
+        // Send to all clients except sender
+        socket.broadcast.emit('update', socket.username + ' has left the server.');
+    });
+
+    // Listen to event 'comment' when user comment
+    socket.on('comment', function(comment) {
+        // Push to comments
+        comments.push(comment);
+        // Send to all clients
+        io.sockets.emit('update-comment', comments);
+    });
 });
 
-// 3. listen port
-server.listen(8080, function() {
-    console.log('Server is running...');
+// Set static folder
+app.use(express.static(__dirname + '/public'));
+
+// Route
+app.get('/', function(req, res) {
+    // Render index.html
+    res.render('index.html');
 });
