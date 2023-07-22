c48217324a50c9a2d6cb5d975c5d1f37d338de48
Saqib Kamal <saqibkamal1994@gmail.com>
2023-07-22 11:40:39 +0000

    created node.js server

--- comments.js
+++ comments.js
@@ -1,20 +1,44 @@
-// create web server with express
-const express = require('express');
-const router = express.Router();
-// import comment controller
-const commentController = require('../controllers/comments');
-// import auth middleware
-const auth = require('../middleware/auth');
+// create a web server with node.js
+// 1. load http module
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var qs = require('querystring');
 
-// create a comment
-router.post('/', auth, commentController.createComment);
-// get all comments
-router.get('/', auth, commentController.getAllComments);
-// get a comment by id
-router.get('/:id', auth, commentController.getOneComment);
-// update a comment
-router.put('/:id', auth, commentController.updateComment);
-// delete a comment
-router.delete('/:id', auth, commentController.deleteComment);
+// 2. create http server
+var server = http.createServer(function(request, response) {
+    var parsedUrl = url.parse(request.url);
+    var resource = parsedUrl.pathname;
+    console.log('resource = ' + resource);
 
-module.exports = router;
+    // if client request '/', response index.html
+    if(resource == '/') {
+        var html = fs.readFileSync('index.html', 'utf-8');
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.end(html);
+    } else if(resource == '/comments') {
+        if(request.method == 'POST') {
+            console.log('POST request');
+            request.on('data', function(data) {
+                var parsedQuery = qs.parse(data.toString());
+                var comment = parsedQuery['comment'];
+                console.log('comment = ' + comment);
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end('var1 = ' + comment);
+            });
+        } else {
+            console.log('GET request');
+            var html = fs.readFileSync('comment.html', 'utf-8');
+            response.writeHead(200, {'Content-Type':'text/html'});
+            response.end(html);
+        }
+    } else {
+        response.writeHead(404, {'Content-Type':'text/html'});
+        response.end('404 Not Found');
+    }
+});
+
+// 3. listen port
+server.listen(8080, function() {
+    console.log('Server is running...');
+});
